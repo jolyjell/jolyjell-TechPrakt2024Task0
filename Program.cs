89b4a4bc218d8832757c82b041e847b6ab0a0c3f
Bohdan Pasichnyk <vt221_pbr@student.ztu.edu.ua>
2024-04-12 17:34:00 +0100

    Коміт c#

---
+++ Program.cs
@@ -0,0 +1,8 @@
+Console.WriteLine("Введіть 1 число");
+string num1 = Console.ReadLine();
+Console.WriteLine("Введіть 2 число");
+string num2 = Console.ReadLine();
+int intnum1 = Int32.Parse(num1);
+int intnum2 = Int32.Parse(num2);
+
+Console.WriteLine(intnum1 + intnum2);
